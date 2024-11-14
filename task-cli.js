bbd9072e002782ca5e6ce42f1fc5043599c210c6
Rishabh Sharma <rishabh.y.sharma@oracle.com>
2024-11-14 12:38:27 +0530

    Updating and deleting Tasks

--- task-cli.js
+++ task-cli.js
@@ -15,17 +15,31 @@ class Task {
       updatedAt === (null || undefined) ? this.createdAt : updatedAt;
   }
   static addTask = taskName => {
-    const newTask = new Task("");
-    const jsonData = JSON.parse(fs.readFileSync("./task.json"))
-    jsonData.push(newTask)
-    fs.writeFileSync('./task.json',JSON.stringify(jsonData))
+    const newTask = new Task(taskName);
+    const jsonData = JSON.parse(fs.readFileSync("./task.json"));
+    jsonData.push(newTask);
+    fs.writeFileSync("./task.json", JSON.stringify(jsonData));
   };
+  static updateTask = (taskId, taskName) => {
+    const jsonData = JSON.parse(fs.readFileSync("./task.json"));
+    jsonData.forEach(element => {
+      if (element.id == taskId) {
+        element.description = taskName;
+        element.updatedAt = new Date().toUTCString();
+      }
+    });
+    fs.writeFileSync("./task.json", JSON.stringify(jsonData));
+  };
+  static deleteTask = (taskId) => {
+    const jsonData = JSON.parse(fs.readFileSync("./task.json"));
+    fs.writeFileSync("./task.json", JSON.stringify(jsonData.filter(item => item.id != taskId)));
+  }
 }
 
 function getLastTaskId() {
-  const data = JSON.parse(fs.readFileSync("./task.json", 'utf-8'))
-  if (data.length===0) return 0
-  return  data[data.length - 1].id
+  const data = JSON.parse(fs.readFileSync("./task.json", "utf-8"));
+  if (data.length === 0) return 0;
+  return data[data.length - 1].id;
 }
 
 if (!process.argv[1].endsWith("task-cli")) {
@@ -33,4 +47,8 @@ if (!process.argv[1].endsWith("task-cli")) {
 } else if (process.argv[2] == "add") {
   Task.addTask(process.argv[3]);
   console.log(`Output: Task added successfully (ID: ${Task.id})`);
+} else if (process.argv[2] == "update") {
+  Task.updateTask(process.argv[3], process.argv[4]);
+} else if (process.argv[2] == "delete") {
+  Task.deleteTask(process.argv[3]);
 }
