4e86e29cf9a0f2e153e6ce7a7bed502c5219b67a
Rishabh Sharma <rishabh.y.sharma@oracle.com>
2024-11-14 12:58:52 +0530

    LIsting tasks

--- task-cli.js
+++ task-cli.js
@@ -20,20 +20,32 @@ class Task {
     jsonData.push(newTask);
     fs.writeFileSync("./task.json", JSON.stringify(jsonData));
   };
-  static updateTask = (taskId, taskName) => {
+  static updateTask = (taskId, taskName, status) => {
     const jsonData = JSON.parse(fs.readFileSync("./task.json"));
     jsonData.forEach(element => {
       if (element.id == taskId) {
-        element.description = taskName;
+        element.description = taskName === null ? element.taskName : taskName;
+        element.status = status === null ? element.status : status;
         element.updatedAt = new Date().toUTCString();
       }
     });
     fs.writeFileSync("./task.json", JSON.stringify(jsonData));
   };
-  static deleteTask = (taskId) => {
+  static deleteTask = taskId => {
     const jsonData = JSON.parse(fs.readFileSync("./task.json"));
-    fs.writeFileSync("./task.json", JSON.stringify(jsonData.filter(item => item.id != taskId)));
-  }
+    fs.writeFileSync(
+      "./task.json",
+      JSON.stringify(jsonData.filter(item => item.id != taskId))
+    );
+  };
+  static listingTasks = status => {
+    const jsonData = JSON.parse(fs.readFileSync("./task.json"));
+    if (status == null) {
+      jsonData.forEach(element => console.log(element));
+    } else {
+      console.log(jsonData.filter(element => element.status == status));
+    }
+  };
 }
 
 function getLastTaskId() {
@@ -48,7 +60,17 @@ if (!process.argv[1].endsWith("task-cli")) {
   Task.addTask(process.argv[3]);
   console.log(`Output: Task added successfully (ID: ${Task.id})`);
 } else if (process.argv[2] == "update") {
-  Task.updateTask(process.argv[3], process.argv[4]);
+  Task.updateTask(process.argv[3], process.argv[4], null);
 } else if (process.argv[2] == "delete") {
   Task.deleteTask(process.argv[3]);
+} else if (
+  process.argv[2] == "mark-in-progress" ||
+  process.argv[2] == "mark-done"
+) {
+  let status = process.argv[2].split(/-(.*)/s);
+  console.log(status);
+  Task.updateTask(process.argv[3], null, status[1]);
+} else if (process.argv[2] == "list") {
+  if (process.argv[3] === undefined) Task.listingTasks(null);
+  else Task.listingTasks(process.argv[3]);
 }
